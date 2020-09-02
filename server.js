8bdbfe0fae15c2cceca79a6e97dda48035d008e6
claytonfaria <claytonfaria@gmail.com>
2020-09-02 23:13:57 +0800

    minor update

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.json({ extended: false }));
 // Server static assets in production
 if (process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname + '/client/build/index.html'));
+  });
 }
 
 // Define Routes
